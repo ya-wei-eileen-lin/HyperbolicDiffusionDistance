0edfbbf508306fe2a628fc275dd6e4a345e53e98
Ya-Wei0 <55050708+Ya-Wei0@users.noreply.github.com>
2023-07-12 11:20:06 +0300

    Update poincare_half_space_model.py

--- poincare_half_space_model.py
+++ poincare_half_space_model.py
@@ -2,5 +2,5 @@ import numpy as np
 from numpy import linalg as LA
 
 def hyp_dis(x,y):
-    return 2 * np.arcsinh(LA.norm(x-y)/(x[-1]*y[-1]))
+    return 2 * np.arcsinh(LA.norm(x-y)/(2*np.sqrt(x[-1]*y[-1])))
 
